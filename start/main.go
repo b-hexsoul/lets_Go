b8c0c3f0d70899dcf9df06858d235ae770a2ac99
b-hexsoul <brandon.hexsel@gmail.com>
2023-01-02 19:16:38 -0800

    init

---
+++ start/main.go
@@ -0,0 +1,14 @@
+package main
+
+// Types of Packages in Golang
+// 1. Executable
+// This type generates a file we can run
+
+// 2. Resuable
+// This type is for code used as "helpers".
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
