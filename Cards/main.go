4351ad55608024bf99f2e83a04d8b8c2ca3917f3
b-hexsoul <brandon.hexsel@gmail.com>
2023-01-03 12:47:27 -0800

    basic syntax

---
+++ Cards/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	// slice is delcared with []type
+	cards := []string{"Ace of Diamonds", newCard()}
+	cards = append(cards, "Six of Spades") // does not modify the cards slice. returns a new slice
+
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
+
+}
+
+// Delcaring Variables
+// var card string
+// var card string = "queen"
+// card := newCard() // will infer the type
+
+// you have to declare the return type from your functions
+func newCard() string {
+	return "Five of Diamonds"
+}
+
+// Array - Fixed length list of items. Must delcare the length at initiation
+// Slice - An array that can grow or shrink. Every element must be of the same type
+// cards := []string{"Ace of Diamonds", newCard()}
+// cards = append(cards, "Six of Diamonds") // does not modify the cards slice but returns a new one
+
+// For Loop iteration over a slice
+/*
+	for index, item := range items {
+		fmt.Println(index, item)
+	}
+*/
--- start/main.go
+++ start/main.go
@@ -4,10 +4,12 @@ package main
 // 1. Executable
 // This type generates a file we can run
 
+// package main is "sacred" and will spit out an executable. Must have a func called main
+
 // 2. Resuable
 // This type is for code used as "helpers".
 
-import "fmt"
+import "fmt" // a part of the standard library in Go
 
 func main() {
 	fmt.Println("hello world")
