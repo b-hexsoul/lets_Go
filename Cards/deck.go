06f0d089f29522c5eb75ad1d6947af8958cf6bd3
b-hexsoul <brandon.hexsel@gmail.com>
2023-01-03 23:17:54 -0800

    slices, multiple return values

--- Cards/deck.go
+++ Cards/deck.go
@@ -1,12 +1,31 @@
 package main // still inside our main package...
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+	cardFaces := []string{"Spades", "Clubs", "Hearts", "Diamonds"}
+	cardValues := []string{"One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"}
+	for _, face := range cardFaces {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+face)
+		}
+	}
+	return cards
+}
+
+// specifying multiple return values with their types in parentheses
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
 func (d deck) print() {
-	for i, card := range d {
-		fmt.Println(i, card)
+	for _, card := range d {
+		fmt.Println(card)
 	}
 }
 
@@ -18,3 +37,5 @@ func (d deck) print() {
 // Make functions with the new "extended" deck type as a Receiver
 // A function with a "Receiver" is like a method - a function that belongs to an instance
 // func (d deck) print() // any variable of type deck now gets the func print...
+
+// go has support for returning multiple values from a function
--- Cards/main.go
+++ Cards/main.go
@@ -1,11 +1,10 @@
 package main
 
 func main() {
-	// slice is delcared with []type
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades") // does not modify the cards slice. returns a new slice
-
-	cards.print()
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 5)
+	hand.print()
+	_, _ = deal(remainingCards, 3)
 }
 
 // Delcaring Variables
@@ -14,9 +13,9 @@ func main() {
 // card := newCard() // will infer the type
 
 // you have to declare the return type from your functions
-func newCard() string {
-	return "Five of Diamonds"
-}
+// func newCard() string {
+// 	return "Five of Diamonds"
+// }
 
 // Array - Fixed length list of items. Must delcare the length at initiation
 // Slice - An array that can grow or shrink. Every element must be of the same type
@@ -25,7 +24,11 @@ func newCard() string {
 
 // For Loop iteration over a slice
 /*
-	for index, item := range items {
-		fmt.Println(index, item)
-	}
+for index, item := range items {
+	fmt.Println(index, item)
+}
 */
+
+// slice is delcared with []type
+// cards := deck{"Ace of Diamonds", newCard()}
+// cards = append(cards, "Six of Spades") // does not modify the cards slice. returns a new slice
