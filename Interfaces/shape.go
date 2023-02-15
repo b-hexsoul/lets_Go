6d8f6280e8a978765dad8552f3ad1dccab7a17a8
b-hexsoul <brandon.hexsel@gmail.com>
2023-02-14 21:03:18 -0800

    shape interface

--- Interfaces/shape.go
+++ Interfaces/shape.go
@@ -3,19 +3,31 @@ package main
 import "fmt"
 
 func main() {
-
+	tri := triangle{base: 10, height: 25}
+	squ := square{sideLength: 10}
+	printArea(tri)
+	printArea(squ)
 }
 
 type shape interface {
-	area() int
+	area() float64
 }
 
 type square struct {
-	sideLength int
+	sideLength float64
 }
 
-func (s square) area() int {
+type triangle struct {
+	base   float64
+	height float64
+}
+
+func (s square) area() float64 {
 	return s.sideLength * s.sideLength
+}
+
+func (t triangle) area() float64 {
+	return t.base * t.height * 0.5
 }
 
 func printArea(s shape) {
